685728f54b5927b1e5ecdcfb21ac367fe6cf3a61
Gb <joaogabrielmcpe110@gmail.com>
2022-10-11 09:49:55 -0300

    adicionando animes para continuação

--- animes_para_assistir.py
+++ animes_para_assistir.py
@@ -1,6 +1,6 @@
 print("seleção de animes para assistir")
 print('''Tokyo Revenge \nMonster \nCyberPunk \nWonder Egg Priority \nDanganrompa
-\nTate No Yushuia 2º Temp \nMob Psicho''')
+\nTate No Yushuia 2º Temp''')
 
 print("seleção de animes para dar continuidade")
-print("One Piece \nRick and Morty")
+print("One Piece \nRick and Morty \nSAO \nMOb Psycho")
